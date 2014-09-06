f76d639e646397b153b70d8f790d678403bb4c10
Eric <erichaydel@gmail.com>
2014-09-06 04:36:43 -0400

    Added autosign in after facebook login (Security risk, sort of)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ SwapBooks::Application.routes.draw do
 
   get "home/show"
   devise_for :users,
-        controllers: { omniauth_callbacks: "omniauth_callbacks" }
+        controllers: {  omniauth_callbacks: "omniauth_callbacks",
+                        confirmations: 'confirmations' }
   root to: "home#show"
 
   get "users/create"
