d2ffeaf936ed69a4161deca7a19f37cc34eabd12
noahpresler <noah@presler.me>
2014-09-06 15:35:24 -0400

    updating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Book.create!(name: 'Programming 101', isbn: ("1" * 10))
-Book.create!(name: 'Arts and Crafts', isbn: ("3" * 10))
-Book.create!(name: 'Intro Biology', isbn: ("2" * 10))
+Book.create!(name: 'Programming 101', isbn: ("1" * 10), author: 'Thomas JakBitch', image_name: 'intropro.jpg')
+Book.create!(name: 'Arts and Crafts', isbn: ("3" * 10), author: 'Krieger School of A/S', image_name:'arttextbook.jpg')
+Book.create!(name: 'Intro Biology', isbn: ("2" * 10), author: 'John hopkin', image_name: 'bio.jpg')
 
 User.create(email: 'dan@test.com')
