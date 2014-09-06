4c217289e6c19758e81e9b6e48740a60781a03af
noahpresler <noah@presler.me>
2014-09-06 15:30:28 -0400

    update image to image_name and db records

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,5 +6,5 @@ class Book < ActiveRecord::Base
   # TODO: Validate that this is isn't in a range, but is either 10 or 13.
   validates :isbn, presence: true, length: { in: 10..13 }, uniqueness: true
   validates :author, presence: true
-  validates :image, presence:true
+  validates :image_name, presence:true
 end
