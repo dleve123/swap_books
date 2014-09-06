71d622b0c48aac85359edb9d6a68dc5392cd1217
noahpresler <noah@presler.me>
2014-09-06 18:14:04 -0400

    login page styled!

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,10 +1,7 @@
 class WelcomesController < ApplicationController
-  before_action :conditionally_redirect_to_books_index
 
   def home
-  end
-
-  def conditionally_redirect_to_books_index
-    redirect_to books_path if current_user
+    redirect_path = current_user ? books_path : new_user_registration_path
+    redirect_to redirect_path
   end
 end
