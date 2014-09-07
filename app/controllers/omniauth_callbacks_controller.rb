b58067fa488014b9880764b6df1a6c552e5df991
Eric <erichaydel@gmail.com>
2014-09-06 22:55:59 -0400

    Picked up some straggling

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
-    binding.pry
 
     if user.persisted?
       flash.notice = "Signed in!"
